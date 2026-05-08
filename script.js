909af2a49280c907ccb1ef8e58e465431a5b115c
Zead <73611847+Zead456@users.noreply.github.com>
2026-05-09 01:41:12 +0300

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let quotesData = [];
 // Load last selected page.
 let selectedPageIndex = 0;
 if (localStorage.getItem("selectedPageIndex") === null) {
-  localStorage.setItem("selectedPageIndex", selectedPageIndex)
+  localStorage.setItem("selectedPageIndex", selectedPageIndex);
 } else {
   selectedPageIndex = localStorage.getItem("selectedPageIndex");
 }
